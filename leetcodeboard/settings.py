0ac610649354b14c94117d5cbd773f4be6e8991c
mablic <maihe88@gmail.com>
2023-07-29 00:28:25 -0500

    standable release

--- leetcodeboard/settings.py
+++ leetcodeboard/settings.py
@@ -9,9 +9,9 @@ PARENT_DIR = os.path.dirname(BASE_DIR)
 mimetypes.add_type("text/javascript", ".js", True)
 
 # Enable HTTPS
-# SECURE_SSL_REDIRECT = True
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -20,13 +20,13 @@ mimetypes.add_type("text/javascript", ".js", True)
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# ALLOWED_HOSTS = ['studygrouppal.com', 'sdashboard.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1']
+# DEBUG = True
+ALLOWED_HOSTS = ['studygrouppal.com', 'sdashboard.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1']
 DISCORD_CLIENT_ID = os.environ.get('DISCORD_CLIENT_ID')
 DISCORD_CLIENT_SECRET = os.environ.get('DISCORD_CLIENT_SECRET')
-# DISCORD_REDIRECT_URI = 'https://' + ALLOWED_HOSTS[0] + '/callback'
-DISCORD_REDIRECT_URI = 'http://' + ALLOWED_HOSTS[0] + ':8000/callback'
+DISCORD_REDIRECT_URI = 'https://' + ALLOWED_HOSTS[0] + '/callback'
+# DISCORD_REDIRECT_URI = 'http://' + ALLOWED_HOSTS[0] + ':8000/callback'
 # Application definition
 
 INSTALLED_APPS = [
