67de3e390e1a0088cc9eaf56d5f8722dc71e30ad
mablic <maihe88@gmail.com>
2023-08-08 00:04:27 -0500

    update w modules

--- leetcodeboard/settings.py
+++ leetcodeboard/settings.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 import os
 import mimetypes
-# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
